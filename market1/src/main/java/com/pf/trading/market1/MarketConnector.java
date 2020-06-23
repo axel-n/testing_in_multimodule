70d1393b8497c1318a80fdae4b9fa5b691152f71
Alex Anoshkin <anoshkin.allex@gmail.com>
2020-06-24 01:14:05 +0300

    test1 teamCity deploy

--- market1/src/main/java/com/pf/trading/market1/MarketConnector.java
+++ market1/src/main/java/com/pf/trading/market1/MarketConnector.java
@@ -32,7 +32,7 @@ public class MarketConnector implements IMarketConnector {
 
     private final IRedisSender redisSender;
 
-    @Scheduled(fixedDelay = 500)
+    @Scheduled(fixedDelay = 499)
     public void sendMockMessages() {
         redisSender.send("some message. now " + System.currentTimeMillis());
     }
